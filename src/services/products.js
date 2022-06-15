b13bb90363a71669d76acd82dc1457a1676acd29
zukhrik <kz_spiky@mail.ru>
2022-06-15 18:35:49 +0500

    fixed bug in service product

--- src/services/products.js
+++ src/services/products.js
@@ -1,6 +1,5 @@
 import {httpGet} from './index'
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getProducts: ({params}) => httpGet({url: '/variations', params})
 }
